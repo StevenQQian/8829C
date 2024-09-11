24c6c6f3b51be0a4f8347ab2164703fb904f7eaf
StevenQQQian <114100226+StevenQQian@users.noreply.github.com>
2024-09-11 15:23:56 -0400

    PID updated

--- 55286A/include/motion.h
+++ 55286A/include/motion.h
@@ -18,6 +18,39 @@ void turnToHeading(double heading, double speedRatio = 1, double reversed = 0, d
     setDrive(0, 0);
 }
 
+void driveStraight(double distance, double speedRatio, double kP = 0, double kI = 0, double kD = 0) {
+    resetPID();
+    double linearError = distance;
+
+    double leftSum = ((lB.position(rotationUnits::rev) * (driveWheelDiameter * M_PI) * (dt_rpm / 600)) + 
+    (lM.position(rotationUnits::rev) * (driveWheelDiameter * M_PI) * (dt_rpm / 600)) + 
+    (lF.position(rotationUnits::rev) * (driveWheelDiameter * M_PI) * (dt_rpm / 600)));
+
+    double leftCurrentReading = leftSum / 3;
+    double leftPrevReading = leftCurrentReading;
+
+    double distaceTraveled = 0;
+    while (fabs(linearError) < 0.2 && lM.velocity(rpm) * driveWheelDiameter / 6000 < 0.02) {
+        // Get drivetrain distance reading
+        leftSum = ((lB.position(rotationUnits::rev) * (driveWheelDiameter * M_PI) * (dt_rpm / 600)) + 
+        (lM.position(rotationUnits::rev) * (driveWheelDiameter * M_PI) * (dt_rpm / 600)) + 
+        (lF.position(rotationUnits::rev) * (driveWheelDiameter * M_PI) * (dt_rpm / 600)));
+
+        leftCurrentReading = leftSum / 3;
+        double deltaLeft = leftCurrentReading - leftPrevReading;
+        distaceTraveled += deltaLeft;
+
+        // Error calculation
+        double error = distance - distaceTraveled;
+        double driveOutput = lateralPID(error, kP, kI, kD);
+        if (fabs(driveOutput) > 100 && driveOutput > 0) driveOutput = 100;
+        else if (fabs(driveOutput) > 100 && driveOutput < 0) driveOutput = -100;
+        setDrive(driveOutput, driveOutput);
+        vexDelay(10);
+    }
+    
+}
+
 
 
 #endif // !MOTION_H
